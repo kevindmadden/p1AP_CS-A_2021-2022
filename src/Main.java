66806b2787046872764cf070d13169e4a5f6789d
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2021-08-19 09:16:55 -0500

    Variables Update

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Print Statements
+        System.out.println("Hello World!");
+        System.out.println("This is a sentence.");
+        System.out.print("Hello World!");
+        System.out.print("This is a sentence.");
+        System.out.print(1000);
+
+        //Variables
+        int number = 9; //integer variable
+        double number2 = 9.6;
+        String word = "This is a sentence in stored in a variable";
+        boolean isItRaining = true;
+        boolean isItCloudy = false;
+        char aSingleCharacter = 'a';
+
+        System.out.println(number);
+        System.out.println(word);
+
+    }
+}
---
+++ src/u01_TheBasics/s02_Operators.java
@@ -0,0 +1,13 @@
+package u01_TheBasics;
+
+public class s02_Operators {
+    public static void main(String[] args) {
+        int addition = 9+3; //addition (12)
+        System.out.println(addition);
+        int subtraction = 75-4; //subtraction (71)
+        int multiplication = 3*8; //multiplication (24)
+        int division = 8/4; //division (this would store 2 inside the division variable)
+        System.out.println(addition*subtraction); //this would print out the result of 12*71
+
+    }
+}
