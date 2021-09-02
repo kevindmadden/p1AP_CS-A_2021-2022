8b65a68e5f6120e1e56896385c97f4ca85958889
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2021-09-02 08:35:51 -0500

    Scale Factor

--- src/u02_UsingObjects/s03_WritingVoidMethods.java
+++ src/u02_UsingObjects/s03_WritingVoidMethods.java
@@ -2,17 +2,20 @@ package u02_UsingObjects;
 
 public class s03_WritingVoidMethods {
     public static void main(String[] args){
-        drawTriangle(0.1,0.4);
-        drawTriangle(.7, 0.2);
-        drawTriangle(.8, 0.3);
-        drawTriangle(.6, 0.2);
+        StdDraw.setScale(100, 100);
+
+        drawTriangle(0.1,0.4,2);
+        drawTriangle(.7, 0.2, 1.5);
+        drawTriangle(.2, 0.3, 1.75);
+        drawTriangle(.1, 0.2, 0.2);
     }
 
     //                                        0.1             0.4
-    public static void drawTriangle(double xShift, double yShift){
-        StdDraw.line(0+xShift, 0+yShift,0.1+xShift,0.2+yShift);
-        StdDraw.line(0.1+xShift,0.2+yShift, 0.2+xShift,0+yShift);
-        StdDraw.line(0.2+xShift, 0+yShift, 0+xShift, 0+yShift);
+    public static void drawTriangle(double xShift, double yShift, double scale){
+        StdDraw.line((0+xShift)*scale, (0+yShift)*scale,(0.1+xShift)*scale,(0.2+yShift)*scale);
+        StdDraw.line((0.1+xShift)*scale,(0.2+yShift)*scale, (0.2+xShift)*scale,(0+yShift)*scale);
+        StdDraw.line((0.2+xShift)*scale,(0+yShift)*scale, (0+xShift)*scale,(0+yShift)*scale);
+
     }
 
 }
