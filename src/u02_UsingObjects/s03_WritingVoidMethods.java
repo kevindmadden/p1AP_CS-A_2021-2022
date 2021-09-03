d960847a324732d8f4a8783a009a88f29c39b178
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2021-09-03 07:51:51 -0500

    AnimationTemplate

--- src/u02_UsingObjects/s03_WritingVoidMethods.java
+++ src/u02_UsingObjects/s03_WritingVoidMethods.java
@@ -2,19 +2,24 @@ package u02_UsingObjects;
 
 public class s03_WritingVoidMethods {
     public static void main(String[] args){
-        StdDraw.setScale(100, 100);
+        StdDraw.setScale(0, 1);
 
-        drawTriangle(0.1,0.4,2);
-        drawTriangle(.7, 0.2, 1.5);
-        drawTriangle(.2, 0.3, 1.75);
-        drawTriangle(.1, 0.2, 0.2);
+        drawTriangleDilateBefore(0.1,0.4,2);
+        drawTriangleDilateAfter(0.1,0.4,2);
     }
 
     //                                        0.1             0.4
-    public static void drawTriangle(double xShift, double yShift, double scale){
-        StdDraw.line((0+xShift)*scale, (0+yShift)*scale,(0.1+xShift)*scale,(0.2+yShift)*scale);
+    public static void drawTriangleDilateAfter(double xShift, double yShift, double scale){
+        StdDraw.line((0+xShift)*scale,(0+yShift)*scale,(0.1+xShift)*scale,(0.2+yShift)*scale);
         StdDraw.line((0.1+xShift)*scale,(0.2+yShift)*scale, (0.2+xShift)*scale,(0+yShift)*scale);
         StdDraw.line((0.2+xShift)*scale,(0+yShift)*scale, (0+xShift)*scale,(0+yShift)*scale);
+
+    }
+
+    public static void drawTriangleDilateBefore(double xShift, double yShift, double scale){
+        StdDraw.line(0*scale+xShift, 0*scale+yShift,0.1*scale+xShift,0.2*scale+yShift);
+        StdDraw.line(0.1*scale+xShift,0.2*scale+yShift, 0.2*scale+xShift,0*scale+yShift);
+        StdDraw.line(0.2*scale+xShift,0*scale+yShift, 0*scale+xShift,0*scale+yShift);
 
     }
 
