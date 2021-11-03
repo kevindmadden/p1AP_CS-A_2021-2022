b32c711a449a721c25669b3be348c71b6f9f5500
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2021-11-03 07:20:18 -0500

    Duck

---
+++ src/u04_Classes/s01_DuckExample/Duck.java
@@ -0,0 +1,20 @@
+package u04_Classes.s01_DuckExample;
+
+public class Duck {
+
+    //instance variables/fields
+    private int numOfTimesHonked;
+    private String speechContent;
+    private boolean isSwimming;
+    private boolean isFlying;
+    private boolean isAlive;
+
+    public Duck(){
+        numOfTimesHonked = 0;
+        speechContent = "I'm a duck.";
+        isSwimming = true;
+        isFlying = false;
+        isAlive = true;
+    }
+
+}
---
+++ src/u04_Classes/s01_DuckExample/Main.java
@@ -0,0 +1,15 @@
+package u04_Classes.s01_DuckExample;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Duck duck1 = new Duck();
+        Duck duck2 = new Duck();
+        //String word = duck1.speechContent;
+        for(int i=0; i<99; i++){
+            //System.out.println(word);
+        }
+
+    }
+}
