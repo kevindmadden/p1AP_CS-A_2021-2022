8d60c2bbbb10eb3a06a4a0b29289c6f1a5e31ec9
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2021-11-18 14:02:50 -0600

    Create Main.java

---
+++ src/u04_Classes/s03_Asteroids/Main.java
@@ -0,0 +1,27 @@
+package u04_Classes.s03_Asteroids;
+
+public class Main {
+
+    /*
+            Brainstorming
+            -Player spaceship
+                -Shape - Collision Detection
+                -Movement
+                    -Making it possible for the ship to rotate
+                        -What direction is it pointing in?
+                    -Keyboard input
+                    -There's a maximum velocity
+                    -Controlling acceleration
+                    -v=v+a*timeElapsed
+                    -
+                -Shooting
+            -Asteroids
+                -Second thing to focus on
+                -First focus on asteroids disappearing when shot
+                    -Then focus on them breaking apart
+            -Alien Ships
+            -Sound effects? (StdDraw doesn't support)
+
+     */
+
+}
