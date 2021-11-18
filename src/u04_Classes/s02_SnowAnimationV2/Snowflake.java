a3c43b3d724f4932be971fd184eeee04bec25134
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2021-11-18 14:02:35 -0600

    Static Snowflake

--- src/u04_Classes/s02_SnowAnimationV2/Snowflake.java
+++ src/u04_Classes/s02_SnowAnimationV2/Snowflake.java
@@ -7,6 +7,8 @@ public class Snowflake {
     private double yVel;
     private double size;
 
+    private static int totalSnowflakeCount = 0;
+
     public Snowflake(){
         yPos = Math.random()*100+25;
         xPos = Math.random()*100;
@@ -16,6 +18,11 @@ public class Snowflake {
 
     public void calculate(double timeElapsed){
         yPos = yPos - yVel*timeElapsed;
+        if(yPos < 0){
+            yPos = 110;
+            totalSnowflakeCount++;
+            //System.out.println(totalSnowflakeCount);
+        }
     }
 
     public void draw(){
@@ -23,5 +30,8 @@ public class Snowflake {
         StdDraw.filledCircle(xPos, yPos, size);
     }
 
+    public static int getTotalSnowflakeCount() {
+        return totalSnowflakeCount;
+    }
 
 }
