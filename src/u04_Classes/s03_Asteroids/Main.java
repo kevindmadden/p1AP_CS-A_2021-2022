d6411419e84a286c1efe7be9ff17f144f9a23bf0
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2021-11-19 08:32:19 -0600

    Asteroids - Day 2 - Rotating Ship

--- src/u04_Classes/s03_Asteroids/Main.java
+++ src/u04_Classes/s03_Asteroids/Main.java
@@ -1,5 +1,7 @@
 package u04_Classes.s03_Asteroids;
 
+import java.awt.event.KeyEvent;
+
 public class Main {
 
     /*
@@ -23,5 +25,31 @@ public class Main {
             -Sound effects? (StdDraw doesn't support)
 
      */
+
+    public static void main(String[] args){
+        StdDraw.setScale(0, 100);
+        StdDraw.enableDoubleBuffering(); //Calling this method stops things from being drawn immediately after a draw method is called. This allows you to call many different draw methods without anything being drawn on the screen; when you call StdDraw.show(), everything will be drawn at once.
+
+        double degreesSpaceship = 0;
+
+        double timeElapsed = 0.017; //0.017 seconds-- this is how long each frame of our animation appears.
+        while(true){
+
+            StdDraw.setPenColor(StdDraw.BLACK);
+            StdDraw.filledSquare(50, 50, 50);
+
+            StdDraw.picture(50, 50, "Spaceship.png", 10, 10, degreesSpaceship);
+
+            if(StdDraw.isKeyPressed(KeyEvent.VK_LEFT)){
+                System.out.println("left arrow is pressed");
+                degreesSpaceship = degreesSpaceship + 1;
+            }
+
+            StdDraw.show(); //Because we have called StdDraw.enableDoubleBuffering(), everything that you draw up until this point will be loaded into java's memory but not actually drawn. Calling StdDraw.draw() then draws everything at once that is loaded into java's memory.
+            StdDraw.pause((int)(timeElapsed*1000)); //You must pass to the pause method the number of milliseconds to pause for; so we multiply by 1000 because our timeElapsed variable is in seconds, not milliseconds.
+            StdDraw.clear(); //This clears everything drawn on the screen. You must redraw the image you wish to display for each frame of an animation
+        }
+    }
+
 
 }
