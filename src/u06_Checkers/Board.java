778132552330347260593c6ff3eb8903f150d81e
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2022-01-11 08:13:01 -0600

    Update to point board needs drawn

--- src/u06_Checkers/Board.java
+++ src/u06_Checkers/Board.java
@@ -6,27 +6,22 @@ public class Board {
 
     Checkers[][] board;
 
-
-
     public Board(){
         board = new Checkers[8][8];
-        board.toString();
+        System.out.println(this.toString());
     }
 
-    //board //{ row1:{..col..}, row2:{..col..},...,{row8:{..col..}}
-    //board[row][col]
 
     @Override
     public String toString() {
-        for(int row = 0; row < board.length; row++){
-            for(int col = 0; col < board[0].length; col++){
-
+        String print = "";
+        for(int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                print += board[row][col] + " ";
             }
-
-            }
+            print += "\n";
         }
-
-        )
+        return print;
     }
 
 }
