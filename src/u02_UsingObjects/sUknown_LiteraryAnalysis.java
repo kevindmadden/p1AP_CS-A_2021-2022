beb0e07064417e68e38b096033234af91b39c79d
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2022-11-28 14:51:25 -0600

    .

--- src/u02_UsingObjects/sUknown_LiteraryAnalysis.java
+++ src/u02_UsingObjects/sUknown_LiteraryAnalysis.java
@@ -11,24 +11,53 @@ public class sUknown_LiteraryAnalysis {
 
     public static void main(String[] args) throws FileNotFoundException {
         String book = "";
-        URL path = sUknown_LiteraryAnalysis.class.getResource("s_unknown_book.txt");
+        URL path = sUknown_LiteraryAnalysis.class.getResource("s_unknown_book2.txt");
         File f = new File(path.getFile());
         //BufferedReader reader = new BufferedReader(new FileReader(f));
 
+        int numCarriageReturns = 0;
         try {
 
             Scanner sc = new Scanner(f);
 
-            String test = "";
+            //String test = "";
             while (sc.hasNextLine()) {
-                test += sc.nextLine();
+                String line = sc.nextLine();
+                book += line+"\n";
+                numCarriageReturns++;
             }
-            System.out.println(test);
+            //System.out.println(test);
             //sc.close();
         }
         catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        //Text
+        System.out.println(book);
+
+
+        //Character count
+        System.out.println("Characters: "+book.length());
+
+        //# of Spaces
+        int numOfSpaces = 0;
+        for(int i=0; i<book.length(); i++){
+            if(book.charAt(i)==' '){
+                numOfSpaces++;
+            }
+        }
+        System.out.println("Num of Spaces "+numOfSpaces);
+
+        for(int i=0; i<book.length()-1; i++){ //Taking into account people who put two spaces after a period.
+            if(book.substring(i,i+2).equals("  ")){
+                numOfSpaces--;
+            }
+        }
+        System.out.println("Num of Spaces "+numOfSpaces);
+
+        //# of words
+
+
 
     }
 
